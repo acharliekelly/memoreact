8543d5ed714a22567202e6db3af9d6110f18e049
Charlie Kelly <charliekelly31415@gmail.com>
2019-07-10 17:50:23 -0400

    set up Flux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,26 @@
-/*
- *  Action Types
- */
+import Dispatcher from '../dispatcher';
+import ActionTypes from '../constants';
 
-export const RESTART_GAME = 'RESTART_GAME';
-export const CHANGE_DECK = 'CHANGE_DECK';
-export const RESIZE_BOARD = 'RESIZE_BOARD';
+class GameActions {
+  resizeBoard (grid) {
+    Dispatcher.dispatch({
+      actionType: ActionTypes.RESIZE_BOARD,
+      payload: grid
+    });
+  }
 
-/*
- * Action Creators
- */
+  changeDeck (deck) {
+    Dispatcher.dispatch({
+      actionType: ActionTypes.CHANGE_DECK,
+      payload: deck
+    });
+  }
 
-export function retartGame () {
-  return { type: RESTART_GAME };
+  restartGame () {
+    Dispatcher.dispatch({
+      actionType: ActionTypes.RESTART_GAME
+    });
+  }
 }
 
-export function changeDeck (deck) {
-  return { type: CHANGE_DECK, deck };
-}
-
-export function resizeBoard (grid) {
-  return { type: RESIZE_BOARD, grid };
-}
+export default new GameActions();
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import gameApp from './reducers';
 import './css/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 
-const store = createStore(gameApp);
-
 const appJsx = (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
 );
 
 ReactDOM.render(appJsx, document.getElementById('root'));
