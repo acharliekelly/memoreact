324fb8333d225ccfe8ead41457db7d6713f3f236
Charlie Kelly <charliekelly31415@gmail.com>
2019-07-11 12:28:10 -0400

    maintain board size

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
 const GameOver = ({ restartGame }) => (
-  <div className="justify-center">
-    <h1>You Win!</h1>
-    <Button variant="primary" onClick={restartGame}>Restart</Button>
+  <div className="game-board">
+    <div className="justify-center">
+      <h1>You Win!</h1>
+      <Button variant="primary" onClick={restartGame}>Restart</Button>
+    </div>
   </div>
 );
 
