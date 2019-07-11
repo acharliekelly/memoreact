c808951078985191c9904d3eada7b0dd581588a6
Charlie Kelly <charliekelly31415@gmail.com>
2019-07-11 12:28:48 -0400

    remove game options from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,59 +1,22 @@
 import React from 'react';
-import { Dropdown } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { Decks } from '../Deck';
-import { Grids } from '../Grid';
+import { Button } from 'react-bootstrap';
 
-const Header = ({ currentDeck, deckChange, currentSize, sizeChange }) => (
+const Header = ({ restartGame }) => (
   <div className="grid-header-container">
     <div className="justify-left logo">
       <img alt="Logo" src="memore.png" />
       <span className="title">Memory Game</span>
     </div>
-    <div className="justify-left">
-      <Dropdown onSelect={sizeChange}>
-        <Dropdown.Toggle variant="success" id="size-menu">
-          {currentSize}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {Grids.map(grid => (
-            <Dropdown.Item
-              key={grid.name}
-              eventKey={grid.name}
-              active={grid.name === currentSize}
-            >
-              {grid.listView}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
-    <div className="justify-left">
-      <Dropdown onSelect={deckChange}>
-        <Dropdown.Toggle variant="primary" id="deck-menu">
-          {currentDeck}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {Decks.map(deck => (
-            <Dropdown.Item
-              key={deck.name}
-              eventKey={deck.name}
-              active={deck.name === currentDeck}
-            >
-              {deck.listView}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
+    <div className="justify-center"></div>
+    <div className="justify-end">
+      <Button variant="primary" onClick={restartGame}>Restart</Button>
     </div>
   </div>
 );
 
 Header.propTypes = {
-  currentSize: PropTypes.string.isRequired,
-  sizeChange: PropTypes.func.isRequired,
-  currentDeck: PropTypes.string.isRequired,
-  deckChange: PropTypes.func.isRequired
+  restartGame: PropTypes.func.isRequired
 };
 
 export default Header;
