28d1efe3f9a150469539b4794196f9b97b4b741b
Charlie Kelly <charliekelly31415@gmail.com>
2020-07-26 00:25:58 -0400

    add Redux DevTools

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './redux/reducers';
-import './css/index.scss';
+import reducer from './redux/reducers/';
+// import './css/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './components/App/';
+import App from './components/App';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const appJsx = (
   <Provider store={store}>
